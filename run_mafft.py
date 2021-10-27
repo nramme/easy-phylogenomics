167abc375397bb024b1b78b0d0dade8d1319aeb8
Nathalia <nathalia.ramme@gmail.com>
2021-10-27 11:11:58 -0300

    Update run_mafft.py

--- run_mafft.py
+++ run_mafft.py
@@ -1,5 +1,17 @@
 import re, os, subprocess, sys
 
+### Command line arguments ###
+
+if len(sys.argv) <= 1:
+    sys.argv.append("-h")
+
+if sys.argv[1] in ["-h", "--help", "getopt", "usage", "-help", "help"]:
+    sys.exit('''
+    Running run_mafft.py
+
+	Mafft: Multiple alignment program for amino acid or nucleotide sequences. [Kazutaka Katoh, Daron M. Standley, MAFFT Multiple Sequence Alignment Software Version 7: Improvements in Performance and Usability, Molecular Biology and Evolution, Volume 30, Issue 4, April 2013, Pages 772–780, https://doi.org/10.1093/molbev/mst010]
+''')
+
 def run_mafft():
     with os.scandir(current_directory) as entries:
         for entry in entries:
